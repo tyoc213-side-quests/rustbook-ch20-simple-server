6642abfd3e89dcc585d40d7208b4412d917b8763
tyoc213 <506234+tyoc213@users.noreply.github.com>
2023-02-08 00:04:39 -0600

    returning an HTML response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::Write};
+use std::{net::{TcpListener, TcpStream}, io::Write, fs};
 use std::io::{BufRead, BufReader};
 
 fn main() {
@@ -15,12 +15,17 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader.lines()
-    .map(|result| result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect();
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
     println!("Resquest {:?}", http_request);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
     stream.write_all(response.as_bytes()).unwrap();
 }
