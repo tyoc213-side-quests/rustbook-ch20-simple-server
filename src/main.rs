ee35bf7a0598b4b4d42d2496a913930372ebe485
tyoc213 <506234+tyoc213@users.noreply.github.com>
2023-02-08 10:46:47 -0600

    one thread per request

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
