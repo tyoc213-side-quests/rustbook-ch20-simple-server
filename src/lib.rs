77f5ce79c6434f2d2b50d637b0651413a8154662
tyoc213 <506234+tyoc213@users.noreply.github.com>
2023-02-08 23:57:59 -0600

    droping sender

--- src/lib.rs
+++ src/lib.rs
@@ -12,28 +12,38 @@ impl Worker {
     // }
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job; executing.");
-
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                },
+                Err(_) => {
+                    println!("worker {id} disconnected; cant execute job");
+                    break;
+                }
+            }
         });
         Worker { id, thread: Some(thread) }
     }
 }
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        drop(self.sender.take());
+
         for worker in &mut self.workers {
+            println!("Shuting down worker {:?}", worker.id);
+
             if let Some(t) = worker.thread.take() {
                 println!("finalizando {:?}", t);
-                t.join().unwrap()
+                t.join().unwrap();
             }
         }
     }
@@ -57,13 +67,13 @@ impl ThreadPool {
         for idx in 0..size {
             workers.push(Worker::new(idx, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, sender }
+        ThreadPool { workers, sender: Some(sender) }
     }
 
     pub fn execute<F>(&self, f: F)
     where F: FnOnce() + Send + 'static,{
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 
     // TODO: pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {...}
